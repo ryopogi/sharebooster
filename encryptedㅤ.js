854b463c22fe5cfaa3e62d44832075c8683761c0
Puyit321 <jkingpuyit@gmail.com>
2025-04-15 01:37:03 +0800

    Update encryptedㅤ.js

--- encryptedㅤ.js
+++ encryptedㅤ.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     
-    const enableScript = true; 
+    const enableScript = false; 
 
     const statusDiv = document.getElementById("script-status");
 
