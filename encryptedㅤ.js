31c1eda6819677f30ae211ab9d7afd2e4963c816
Puyit321 <jkingpuyit@gmail.com>
2025-04-14 11:19:55 +0800

    Update encryptedㅤ.js

--- encryptedㅤ.js
+++ encryptedㅤ.js
@@ -1,35 +1,35 @@
-var loadExternalScript = true; // Editable
+var loadExternalScript = true; // Editable toggle
 
 (function(){
-    const keys = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
-    function customBase64Decode(input) {
-        let str = '', chr1, chr2, chr3, enc1, enc2, enc3, enc4, i = 0;
-        input = input.replace(/[^A-Za-z0-9\+\/\=]/g, '');
-        while (i < input.length) {
-            enc1 = keys.indexOf(input.charAt(i++));
-            enc2 = keys.indexOf(input.charAt(i++));
-            enc3 = keys.indexOf(input.charAt(i++));
-            enc4 = keys.indexOf(input.charAt(i++));
-            chr1 = (enc1 << 2) | (enc2 >> 4);
-            chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-            chr3 = ((enc3 & 3) << 6) | enc4;
-            str += String.fromCharCode(chr1);
-            if (enc3 != 64) str += String.fromCharCode(chr2);
-            if (enc4 != 64) str += String.fromCharCode(chr3);
-        }
-        return str;
-    }
+    const _0x4e9f = [
+        'createElement',
+        'script',
+        'src',
+        'appendChild',
+        'head',
+        'getElementById',
+        'style',
+        'display',
+        'none',
+        'textContent',
+        'Script is disabled'
+    ];
 
-    const encodedURL = "aHR0cHM6Ly9zaGFyZWJvb3N0ZXIubmVvY2l0aWVzLm9yZy9zY3JpcHQlRTMlODUlQTRqcw=="; // encoded https://sharebooster.neocities.org/script%E3%85%A4.js
-    const url = customBase64Decode(encodedURL);
+    const part1 = 'https://sh';
+    const part2 = 'arebooster.';
+    const part3 = 'neocities.org/';
+    const part4 = 'script';
+    const part5 = '%E3%85%A4.js';
 
-    if (loadExternalScript) {
-        var s = document.createElement('script');
-        s.src = url;
-        document.head.appendChild(s);
-    } else {
-        var statusDiv = document.getElementById("scriptStatus");
-        statusDiv.style.display = "block";
-        statusDiv.textContent = "Script is disabled";
+    const finalURL = part1 + part2 + part3 + part4 + part5;
+
+    if(loadExternalScript){
+        var s = document[_0x4e9f[0]](_0x4e9f[1]);
+        s[_0x4e9f[2]] = finalURL;
+        document[_0x4e9f[4]][_0x4e9f[3]](s);
+    }else{
+        var d = document[_0x4e9f[5]]('scriptStatus');
+        d[_0x4e9f[6]][_0x4e9f[7]] = _0x4e9f[8];
+        d[_0x4e9f[9]] = _0x4e9f[10];
     }
 })();
