bc7d749f37fcfd4f14a2749890756bec6b1dfc69
Puyit321 <jkingpuyit@gmail.com>
2025-04-16 15:54:34 +0800

    Update encryptedㅤ.js

--- encryptedㅤ.js
+++ encryptedㅤ.js
@@ -14,7 +14,7 @@ const enableScript = true;
           "sharebooster",
           ".neocities.",
           "org/",
-          "scrippt",
+          "scripptt",
           "ㅤ",
           ".js"
         ];
