232d94183a8168f84ec9415d61dc8cf7381aa5b5
Puyit321 <jkingpuyit@gmail.com>
2025-05-13 20:55:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-// script.js
-console.log("Loading additional components...");
-
-const realScript = document.createElement('script');
-realScript.src = 'script2.js';  // real logic
-document.head.appendChild(realScript);
+if (!document.getElementById('myScript')) {
+  const script = document.createElement('script');
+  script.src = 'script2.js';
+  script.id = 'myScript';
+  document.head.appendChild(script);
+}
