83485540af18e6d78a727998ab8110e37a8c70bb
Puyit321 <jkingpuyit@gmail.com>
2025-05-16 17:52:10 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function loadJSFile(src) {
+  const script = document.createElement('script');
+  script.src = src;
+  script.type = 'text/javascript';
+  script.onload = () => {
+    console.log(`${src} loaded successfully.`);
+  };
+  script.onerror = () => {
+    console.error(`Failed to load ${src}`);
+  };
+  document.head.appendChild(script);
+}
+
+// Usage
+loadJSFile('script.js');
