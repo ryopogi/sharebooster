1959c7bbbeaf0596e4a24041668e91723187448e
Puyit321 <jkingpuyit@gmail.com>
2025-05-31 20:04:47 +0800

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-const response = await fetch('/api/submit', {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({ token, url, amount, cookie, interval, server: serverValue })
-});
